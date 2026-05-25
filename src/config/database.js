95e70046e04251bdfac265fe1a7e3e51a3207b7c
Gabriel Vidigal <vidigalgabriel@gmail.com>
2026-05-25 19:45:02 -0300

    Finalizando server e testando conexao com banco

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+async function connectDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log('MongoDB conectado');
+  } catch (error) {
+    console.error('Erro ao conectar MongoDB', error);
+
+    process.exit(1);
+  }
+}
+
+module.exports = connectDatabase;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 
 const app = require('./app');
+const connectDatabase = require('./config/database');
 
 const PORT = process.env.PORT || 3000;
+
+connectDatabase();
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
