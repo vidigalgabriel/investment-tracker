f6d6db903b9823fd2e1c73097170a3f595e16f28
Gabriel Vidigal <vidigalgabriel@gmail.com>
2026-05-25 20:38:28 -0300

    adiciona middleware global para tratamento de erros

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const routes = require('./routes');
+const errorHandler = require('./middlewares/error.middleware');
 
 const app = express();
 
@@ -7,9 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json({
-    message: 'Investment Tracker API'
-  });
+  res.json({ message: 'Investment Tracker API' });
 });
+
+
+app.use('/api', routes);
+
+app.use(errorHandler);
 
 module.exports = app;
