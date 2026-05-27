95a6c1786149f1e2994801ff91625373273832d1
Gabriel Vidigal <vidigalgabriel@gmail.com>
2026-05-26 21:38:06 -0300

    config da api com swagger

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./config/swagger.json');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/error.middleware');
 
@@ -8,13 +10,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 app.get('/', (req, res) => {
   res.json({ message: 'Investment Tracker API' });
 });
 
-
 app.use('/api', routes);
-
 app.use(errorHandler);
 
 module.exports = app;
