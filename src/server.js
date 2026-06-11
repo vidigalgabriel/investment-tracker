2462c7f2e31d4f4d4363f9c9933c849eab6c372a
Gabriel Vidigal <vidigalgabriel@gmail.com>
2026-06-11 09:44:30 -0300

    fix package json

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const app = require('./app');
 const connectDatabase = require('./config/database');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || process.env.PORT;
 
 connectDatabase();
 
